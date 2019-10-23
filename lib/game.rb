8345b9cfdad3cc4d709ac64c97c32471c3d8c82e
Olivia Kesler <oakesler@protonmail.ch>
2019-10-23 14:42:55 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -70,15 +70,14 @@ class Game
   
   def turn
     a = current_player.move(board)
+    if board.valid_move?(a)
+      board.update(a, current_player)
+      board.display
+    end
     while !board.valid_move?(a)
     a = current_player.move(board)
     board.update(a, current_player)
     board.display
-    break 
-    if board.valid_move?(a)
-      board.update(a, current_player)
-      board.display
-      end
     end
   end
   
